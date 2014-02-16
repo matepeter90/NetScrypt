5b5de31f61238b34a9d810f1ee109e4816d68c91
Julien Roncaglia <julien@roncaglia.fr>
2014-02-16 13:04:22 +0100

    Fill the assembly attributes

--- BlackFox.Cryptography.NetScrypt.Tests/Properties/AssemblyInfo.cs
+++ BlackFox.Cryptography.NetScrypt.Tests/Properties/AssemblyInfo.cs
@@ -1,36 +1,10 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("BlackFox.Cryptography.NetScrypt.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BlackFox.Cryptography.NetScrypt.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("NetScrypt Tests")]
+[assembly: AssemblyDescription("NUnit tests for NetScrypt")]
+[assembly: AssemblyCompany("Julien Roncaglia")]
+[assembly: AssemblyProduct("NetScrypt")]
+[assembly: AssemblyCopyright("Copyright © Julien Roncaglia 2014")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("62c2ee71-20e3-4f56-9ea4-bfc6ee2f9c22")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
